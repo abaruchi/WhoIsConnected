186e6f7781c24e04b60aac20c8f5825b6c42ec37
Artur Baruchi <artur.baruchi@gmail.com>
2019-01-24 00:24:14 -0200

    Adds implementations of models

---
+++ core/models.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from uuid import UUID
+
+from pony.orm import Database, Optional, PrimaryKey, Required, Set, sql_debug
+
+
+db = Database()
+db.bind('sqlite',
+        'whoisconnected',
+        create_db=True)
+
+
+class Device(db.Entity):
+    """
+    Class to handle Devices running
+    """
+    mac_addr = PrimaryKey(str, auto=True)
+    name = Required(str)
+    eth_vendor = Optional(str)
+    cur_status = Required(str)
+    connect_times = Set('ConnectTime')
+
+
+class ConnectTime(db.Entity):
+    """
+    Class to keep tracking of status changing over time
+    """
+    id = PrimaryKey(UUID, auto=True)
+    lease_time = Required(str)
+    time = Required(datetime)
+    transition = Optional(int, default=0)
+    person = Required(Device)
+
+
+db.generate_mapping(create_tables=True)
