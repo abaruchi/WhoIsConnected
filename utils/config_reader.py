7055fa1d298680c1e0cd65f23656d7b9de2a76d0
Artur Baruchi <artur.baruchi@gmail.com>
2019-01-28 18:37:08 -0200

    Removes ‘self’ from config_data dict

--- utils/config_reader.py
+++ utils/config_reader.py
@@ -11,40 +11,40 @@ class ConfigData(object):
         self.config_file = config_file
         self.config = RawConfigParser()
         self.config.read(self.config_file)
-        self.config_data = dict()
 
     def get_dhcp_info(self):
-        self.config_data = {
+        config_data = {
             'dhcp': dict()
         }
-        self.config_data['dhcp']['lease_file'] = self.config.get(
+        config_data['dhcp']['lease_file'] = self.config.get(
                                                         'dhcp',
                                                         'LEASE_FILE')
-        self.config_data['dhcp']['know_devices'] = self.config.get(
+        config_data['dhcp']['know_devices'] = self.config.get(
                                                         'dhcp',
                                                         'KNOW_DEVICES')
-        return self.config_data
+        return config_data
 
     def get_gmail_info(self):
-        self.config_data = {
+        config_data = {
             'gmail': dict()
         }
 
-        self.config_data['gmail']['user'] = self.config.get(
+        config_data['gmail']['user'] = self.config.get(
                                                 'gmail',
                                                 'USER')
-        self.config_data['gmail']['pass'] = self.config.get(
+        config_data['gmail']['pass'] = self.config.get(
                                                 'gmail',
                                                 'PASS')
-        self.config_data['gmail']['subject'] = self.config.get(
+        config_data['gmail']['subject'] = self.config.get(
                                                 'gmail',
                                                 'SUBJECT')
+        return config_data
 
     def get_network_info(self):
-        self.config_data = {
+        config_data = {
             'network': dict()
         }
-
-        self.config_data['network']['mac_url'] = self.config.get(
+        config_data['network']['mac_url'] = self.config.get(
                                                 'network',
                                                 'MAC_URL')
+        return config_data
