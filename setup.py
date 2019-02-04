77e8b17eeaee6993db8724cb3112e83dc07106a0
Artur Baruchi <artur.baruchi@gmail.com>
2019-02-04 13:23:45 -0200

    Adds setup file to the project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='WhoIsConnected',
+    version='0.1',
+    packages=['tests', 'utils'],
+    url='https://github.com/abaruchi/WhoIsConnected',
+    license='MIT',
+    author='Artur Baruchi',
+    author_email='artur.baruchi@gmail.com',
+    description=''
+)
