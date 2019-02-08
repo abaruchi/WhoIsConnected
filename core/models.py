cf73052ea16f8af440b193b6d8ea122349a3e31b
Artur Baruchi <artur.baruchi@gmail.com>
2019-02-08 20:00:14 -0200

    Typo fixes + Fields type correction

--- core/models.py
+++ core/models.py
@@ -3,12 +3,11 @@ from uuid import UUID
 
 from pony.orm import Database, Optional, PrimaryKey, Required, Set, sql_debug
 
+
 db = Database()
 db.bind('sqlite',
         'whoisconnected',
         create_db=True)
-
-db = Database()
 
 
 class Device(db.Entity):
@@ -22,7 +21,7 @@ class Device(db.Entity):
 
 class ConnectTime(db.Entity):
     id = PrimaryKey(UUID, auto=True)
-    lease_time = Optional(str)
+    lease_time = Optional(int)
     time = Required(datetime)
     transition = Optional(str)  # 0 - Offline to Online, 1 - Online to Offline
     device = Required(Device)
@@ -30,10 +29,9 @@ class ConnectTime(db.Entity):
 
 class IPLease(db.Entity):
     id = PrimaryKey(UUID, auto=True)
-    IPv4Addr = Optional(str, nullable=True)
-    IPv6Addr = Optional(str, nullable=True)
-    # Indicates if this IPLease is the current assigned IP Addr
-    Current = Optional(bool)
+    ipv4 = Optional(str, nullable=True)
+    ipv6 = Optional(str, nullable=True)
+    current = Optional(bool)
     device = Required(Device)
 
 
