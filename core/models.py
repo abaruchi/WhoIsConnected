3f31d5b12a3ccbe3abe8c799c736a8118cffd971
Artur Baruchi <artur.baruchi@gmail.com>
2019-02-13 23:50:41 -0200

    Refactor models.py to be able to use several databases - issue #23

--- core/models.py
+++ core/models.py
@@ -1,38 +1,37 @@
 from datetime import datetime
 from uuid import UUID
 
-from pony.orm import Database, Optional, PrimaryKey, Required, Set, sql_debug
+from pony.orm import Database, Optional, PrimaryKey, Required, Set
 
 
-db = Database()
-db.bind('sqlite',
-        'whoisconnected',
-        create_db=True)
+def define_entities(db):
+
+    class Device(db.Entity):
+        mac_addr = PrimaryKey(str)
+        name = Optional(str)
+        eth_vendor = Optional(str)
+        cur_status = Optional(str)
+        connect_times = Set('ConnectTime')
+        ip_leases = Set('IPLease')
+
+    class ConnectTime(db.Entity):
+        id = PrimaryKey(UUID, auto=True)
+        lease_time = Optional(int)
+        time = Required(datetime)
+        transition = Optional(int)
+        device = Required(Device)
+
+    class IPLease(db.Entity):
+        id = PrimaryKey(UUID, auto=True)
+        ipv4 = Optional(str, nullable=True)
+        ipv6 = Optional(str, nullable=True)
+        current = Optional(bool)
+        device = Required(Device)
 
 
-class Device(db.Entity):
-    mac_addr = PrimaryKey(str)
-    name = Optional(str)
-    eth_vendor = Optional(str)
-    cur_status = Optional(str)
-    connect_times = Set('ConnectTime')
-    ip_leases = Set('IPLease')
+def define_db(**db_params):
+    db = Database(**db_params)
+    define_entities(db)
+    db.generate_mapping(create_tables=True)
 
-
-class ConnectTime(db.Entity):
-    id = PrimaryKey(UUID, auto=True)
-    lease_time = Optional(int)
-    time = Required(datetime)
-    transition = Optional(int)
-    device = Required(Device)
-
-
-class IPLease(db.Entity):
-    id = PrimaryKey(UUID, auto=True)
-    ipv4 = Optional(str, nullable=True)
-    ipv6 = Optional(str, nullable=True)
-    current = Optional(bool)
-    device = Required(Device)
-
-
-db.generate_mapping(create_tables=True)
+    return db
