55b7cefa6fa0b84e59a021d89bac03e00575c687
Artur Baruchi <artur.baruchi@gmail.com>
2019-03-03 21:17:38 -0300

    Refactoring email routines to send email using boto3 - issue #35

--- utils/email.py
+++ utils/email.py
@@ -1,8 +1,6 @@
 """ Routines and Classes to inform the user about new Device on Network
 """
-import smtplib
 import boto3
-
 from pony.orm import db_session
 
 from core.views import last_ip_lease
@@ -18,94 +16,58 @@ class SendMAIL(object):
         self.devices_data = devices_data
         self.db = db
 
-        user = self.config_email['access_key']
-        password = self.config_email['secret_key']
+        user = self.config_email['email']['access_key']
+        password = self.config_email['email']['secret_key']
 
         self.client = boto3.client(
             service_name='ses',
-            region_name='eu',
-            user=user,
-            pw=password
+            region_name='eu-west-1',
+            aws_access_key_id=user,
+            aws_secret_access_key=password
         )
 
     @db_session
     def send_message(self, message=None):
-        body = ""
         destination = {
-            'ToAddresses': self.config_email['dest'].replace(' ','').split(',')
+            'ToAddresses': self.config_email['email']['dest'].
+                replace(' ', '').split(',')
         }
-        source = 'me@abaruchi.com'
+        source = self.config_email['email']['sender']
 
         if message is None:
-            pass
+            body = ''
+            for device_stat in self.devices_data.keys():
+                if device_stat == "changed_devices":
+                    body += "<p><b>Devices Changed Status:</b><br/>"
+                    body += "<ul>"
+                    for i, device in enumerate(self.devices_data[device_stat]):
+                        ip_lease = last_ip_lease(device, self.db)
+                        body += "<li> Device {}: {} / {} / {}".format(
+                            i, device.name,
+                            ip_lease.ipv4, device.cur_status)
+                    body += "</ul>"
+                    body += "</p>"
+
+                if device_stat == "new_devices":
+                    body += "<p><b>New Devices:</b><br />"
+                    body += "<ul>"
+                    for i, device in enumerate(self.devices_data[device_stat]):
+                        ip_lease = last_ip_lease(device, self.db)
+                        body += "<li> Device {}: {} / {} / {}".format(
+                            i, device.name,
+                            ip_lease.ipv4, device.cur_status)
+                    body += "</ul>"
+                    body += "</p>"
 
         else:
             body = message
 
-
-
-
-
-    #
-    #     self.destination = {
-    #         'ToAdress': self.config_email['dest'].replace(' ','').split(',')
-    #     }
-    #
-    #
-    #
-    #
-    #
-    #     self.server = 'smtp.gmail.com'
-    #     self.port = 587
-    #     session = smtplib.SMTP(
-    #         self.server, self.port
-    #     )
-    #     session.ehlo()
-    #     session.starttls()
-    #     session.ehlo
-    #     session.login(self.config_email['gmail']['user'],
-    #                   self.config_email['gmail']['pass'])
-    #     self.session = session
-    #
-    # @db_session
-    # def send_message(self, message=None):
-    #
-    #     body = ""
-    #     if message is None:
-    #         for device_stat in self.devices_data.keys():
-    #             if device_stat == "changed_devices":
-    #                 body += "<p><b>Devices Changed Status:</b><br />"
-    #                 body += "<ul>"
-    #                 for i, device in enumerate(self.devices_data[device_stat]):
-    #                     ip_lease = last_ip_lease(device, self.db)
-    #                     body += "<li> Device {}: {} / {} / {}".format(
-    #                         i, device.name,
-    #                         ip_lease.ipv4, device.cur_status)
-    #                 body += "</ul>"
-    #                 body += "</p>"
-    #
-    #             if device_stat == "new_devices":
-    #                 body += "<p><b>New Devices:</b><br />"
-    #                 body += "<ul>"
-    #                 for i, device in enumerate(self.devices_data[device_stat]):
-    #                     ip_lease = last_ip_lease(device, self.db)
-    #                     body += "<li> Device {}: {} / {} / {}".format(
-    #                         i, device.name,
-    #                         ip_lease.ipv4, device.cur_status)
-    #                 body += "</ul>"
-    #                 body += "</p>"
-    #     else:
-    #         body = message
-    #
-    #     headers = [
-    #         "From: " + self.config_email['gmail']['user'],
-    #         "Subject: " + self.config_email['gmail']['subject'],
-    #         "To: " + self.config_email['gmail']['dest'],
-    #         "Content-Type: text/html"
-    #     ]
-    #     headers = "\r\n".join(headers)
-    #     self.session.sendmail(
-    #         self.config_email['gmail']['user'],
-    #         self.config_email['gmail']['user'],
-    #         headers + "\r\n\r\n" + body
-    #     )
+        message_dict = {
+            'Subject': {'Data': self.config_email['email']['subject']},
+            'Body': {'Html': {'Data': body}}
+        }
+        self.client.send_email(
+            Source=source,
+            Destination=destination,
+            Message=message_dict
+        )
