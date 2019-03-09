38c583e220968e583e0e5925d5ce42abd6f347e9
Artur Baruchi <artur.baruchi@gmail.com>
2019-03-09 00:41:44 -0300

    Adds new method to render email

--- utils/email.py
+++ utils/email.py
@@ -1,5 +1,7 @@
 """ Routines and Classes to inform the user about new Device on Network
 """
+import os
+
 import boto3
 from jinja2 import Environment, FileSystemLoader
 from pony.orm import db_session
@@ -9,17 +11,16 @@ from core.views import last_ip_lease
 from .config_reader import ConfigData
 
 
-def email_body_generator(devices_dict):
+def email_body_generator(device_dict):
     """
 
     :param devices_dict:
     :return:
     """
-    template_search_dir = "./templates/"
-    template_loader = FileSystemLoader(searchpath=template_search_dir)
-    template_env = Environment(loader=template_loader)
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    j2_env = Environment(loader=FileSystemLoader(this_dir + '/templates/'))
 
-    return template_env.get_template(TEMPLATE_FILE)
+    return j2_env.get_template('email_template.html').render(items=device_dict)
 
 
 @db_session
@@ -41,38 +42,46 @@ class SendMAIL(object):
         )
 
     @db_session
+    def __build_dict_to_render(self):
+        """
+        Creates a dict used by Jinja2 template to render the email that will be
+        used to send to the user.
+
+        :return: A dict to Jinja2 templates render
+        """
+        dict_to_render = {
+            'changed_devices': dict(),
+            'new_devices': dict()
+        }
+
+        for device_stat in self.devices_data.keys():
+            if device_stat == 'changed_devices':
+                for device in self.devices_data[device_stat]:
+                    ip_lease = last_ip_lease(device, self.db)
+                    dict_to_render[device_stat][device.mac_addr] = {
+                        'ip': ip_lease.ipv4,
+                        'name': device.name,
+                        'status': device.cur_status
+                    }
+            if device_stat == 'new_devices':
+                for device in self.devices_data[device_stat]:
+                    ip_lease = last_ip_lease(device, self.db)
+                    dict_to_render[device_stat][device.mac_addr] = {
+                        'ip': ip_lease.ipv4,
+                        'name': device.name,
+                        'status': device.cur_status
+                    }
+        return dict_to_render
+
     def send_message(self, message=None):
         destination = {
             'ToAddresses': self.config_email['email']['dest'].
                 replace(' ', '').split(',')
         }
         source = self.config_email['email']['sender']
-
+        dict_to_render = self.__build_dict_to_render()
         if message is None:
-            body = ''
-            for device_stat in self.devices_data.keys():
-                if device_stat == "changed_devices":
-                    body += "<p><b>Devices Changed Status:</b><br/>"
-                    body += "<ul>"
-                    for i, device in enumerate(self.devices_data[device_stat]):
-                        ip_lease = last_ip_lease(device, self.db)
-                        body += "<li> Device {}: {} / {} / {}".format(
-                            i, device.name,
-                            ip_lease.ipv4, device.cur_status)
-                    body += "</ul>"
-                    body += "</p>"
-
-                if device_stat == "new_devices":
-                    body += "<p><b>New Devices:</b><br />"
-                    body += "<ul>"
-                    for i, device in enumerate(self.devices_data[device_stat]):
-                        ip_lease = last_ip_lease(device, self.db)
-                        body += "<li> Device {}: {} / {} / {}".format(
-                            i, device.name,
-                            ip_lease.ipv4, device.cur_status)
-                    body += "</ul>"
-                    body += "</p>"
-
+            body = email_body_generator(dict_to_render)
         else:
             body = message
 
